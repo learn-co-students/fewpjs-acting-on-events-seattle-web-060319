4b6a52b3eb3cc25f57d5ac62d42d34360e05d097
Rohit <rohitpratti@gmail.com>
2019-07-16 17:02:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+var dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 3}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 3}px`;
+  }
+}
+
+
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  else if (e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+});
