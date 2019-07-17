d1651287f4d4c18d1a24e900150b6deb146bee44
Rohit <rohitpratti@gmail.com>
2019-07-16 18:00:40 -0700

    Done.

--- index.js
+++ index.js
@@ -18,13 +18,118 @@ function moveDodgerRight() {
   }
 }
 
+function moveDodgerUp() {
+  var bottomNumbers = dodger.style.bottom.replace("px", "");
+  var bottom = parseInt(bottomNumbers, 10);
 
+  if (bottom < 380) {
+    dodger.style.bottom = `${bottom + 3}px`;
+  }
+}
+
+function moveDodgerDown() {
+  var bottomNumbers = dodger.style.bottom.replace("px", "");
+  var bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom > 0) {
+    dodger.style.bottom = `${bottom - 3}px`;
+  }
+}
+function moveDodgerUR() {
+  var bottomNumbers = dodger.style.bottom.replace("px", "");
+  var bottom = parseInt(bottomNumbers, 10);
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+
+    dodger.style.bottom = `${bottom + 3}px`;
+    dodger.style.left = `${left + 3}px`;
+}
+function moveDodgerUL() {
+  var bottomNumbers = dodger.style.bottom.replace("px", "");
+  var bottom = parseInt(bottomNumbers, 10);
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+
+    dodger.style.bottom = `${bottom + 3}px`;
+    dodger.style.left = `${left - 3}px`;
+}
+
+function moveDodgerDR() {
+  var bottomNumbers = dodger.style.bottom.replace("px", "");
+  var bottom = parseInt(bottomNumbers, 10);
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+
+    dodger.style.bottom = `${bottom - 3}px`;
+    dodger.style.left = `${left + 3}px`;
+}
+
+function moveDodgerDL() {
+  var bottomNumbers = dodger.style.bottom.replace("px", "");
+  var bottom = parseInt(bottomNumbers, 10);
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 10);
+
+
+    dodger.style.bottom = `${bottom - 3}px`;
+    dodger.style.left = `${left - 3}px`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let keys = {"up" : false, "down" : false, "right" : false, "left" : false};
 
 document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+  console.log(e.key)
+  if(e.key === "ArrowUp"){
+    keys.up = true
   }
-  else if (e.key === "ArrowRight"){
-    moveDodgerRight();
+  if(e.key === "ArrowRight"){
+    keys.right = true;
   }
+  if(e.key === "ArrowLeft"){
+    keys.left = true;
+  }
+  if(e.key === "ArrowDown"){
+    keys.down = true;
+  }
+
+  if (keys.up === true && keys.right === true && keys.left === false && keys.down === false){
+    moveDodgerUR();
+  }
+  if (keys.up === true && keys.right === false && keys.left === true && keys.down === false){
+    moveDodgerUL();
+  }
+  if (keys.up === false && keys.right === true && keys.left === false && keys.down === true){
+    moveDodgerDR();
+  }
+  if (keys.up === false && keys.right === false && keys.left === true && keys.down === true){
+    moveDodgerDL();
+  }
+    console.log(keys);
+});
+
+document.addEventListener("keyup", function(e){
+  keys = {"up" : false, "down" : false, "right" : false, "left" : false};
+
 });
